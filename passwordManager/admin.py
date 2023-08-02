96f8a2847107c6e716ef7e9e2a87a3941c839d1a
oumaima ouyassine <83994637+oumaimaoys@users.noreply.github.com>
2023-08-02 18:18:01 +0100

    set up admin page

--- passwordManager/admin.py
+++ passwordManager/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
-
+from .models import User, Platform, Accounts
 # Register your models here.
+
+class ManageUser(admin.ModelAdmin):
+    list_display =  ["id","first_name", "last_name","user_name", "password"]
+
+class ManagePlatform(admin.ModelAdmin):
+    list_display =  ["id","platform_name", "platform_link", "accounts_created_on_platform"]
+
+    def accounts_created_on_platform(self,platform_id):
+        return Accounts.objects.filter(platform=platform_id).count()
+         
+
+
+class ManagerAccount(admin.ModelAdmin):
+    list_display =  ["id","platform", "user"]
+
+    def format_user_name():
+        pass
+
+    def format_platform_name():
+        pass
+
+
+admin.site.register(User, ManageUser)
+admin.site.register(Platform, ManagePlatform)
+admin.site.register(Accounts, ManagerAccount)
--- passwordManager/models.py
+++ passwordManager/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    user_name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+
+
+
+class Platform(models.Model):
+    platform_name = models.CharField(max_length=255)
+    platform_link = models.CharField(max_length=255)
+
+
+class Accounts(models.Model):
+    platform = models.ForeignKey(Platform, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+
+"""class Admins(models.Model):
+    pass"""
