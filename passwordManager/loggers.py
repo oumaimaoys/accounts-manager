656f542885ab148c63d64a2164a2e1a0d06185dd
oumaima ouyassine <83994637+oumaimaoys@users.noreply.github.com>
2023-08-04 16:15:25 +0100

    downloaded gitlab package

--- passwordManager/loggers.py
+++ passwordManager/loggers.py
@@ -2,14 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import requests
+import gitlab
 
 
 # an abstract class that has abstract methods for an abstract platform
 
-class Logger():
+class Logger(): #an abstract logger
+    platform_api_url = ""
+    platform_api_token = ""
+    platform_sign_up_url = ""
+
 
     def create_account_with_scrapper(self):
         pass
 
     def create_account_with_api(self):
         pass
+
+
+class GitlabLogger(Logger):
+    platform_api_token = "glpat-wtAzNm4yqvrwGmDUsXGc"
+    platform_api_url = "https://gitlab.example.com/" # make sure ot change this later
--- passwordManager/tests.py
+++ passwordManager/tests.py
@@ -1,3 +1,4 @@
 from django.test import TestCase
 
 # Create your tests here.
+# the website set up mock up for testing 
