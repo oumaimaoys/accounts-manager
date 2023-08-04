3ae3160fe3a297bd1d5347f94638b5a171534986
oumaima ouyassine <83994637+oumaimaoys@users.noreply.github.com>
2023-08-04 11:28:56 +0100

    downloaded new dependancy requests, and initiated logger class

--- passwordManager/loggers.py
+++ passwordManager/loggers.py
@@ -1,3 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import requests
+
+
+# an abstract class that has abstract methods for an abstract platform
+
+class Logger():
+
+    def create_account_with_scrapper(self):
+        pass
+
+    def create_account_with_api(self):
+        pass
