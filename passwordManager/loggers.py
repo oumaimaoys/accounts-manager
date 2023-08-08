229294dc4b2f69b6d82e7ac4c3bd5db604f9a89b
oumaima ouyassine <83994637+oumaimaoys@users.noreply.github.com>
2023-08-08 12:28:59 +0100

    dowbnloaded mattermost package and added other loggers

--- passwordManager/loggers.py
+++ passwordManager/loggers.py
@@ -1,34 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import requests
+import mattermost
 import gitlab
 
 
 # an abstract class that has abstract methods for an abstract platform
 
 class Logger(): #an abstract logger
-    platform_api_url = ""
-    platform_api_token = ""
-    platform_sign_up_url = ""
-
-    def create_account_with_api(self):
-        pass
+    def __init__(self, token, url):
+        self.platform_api_token = token
+        self.platform_api_url = url
 
 
 class GitlabLogger(Logger):
     def __init__(self, token, url):
         super().__init__()
-        self.platform_api_token = token
-        self.platform_api_url = url
         self.gl = gitlab.Gitlab(url=self.platform_api_url, private_token=self.platform_api_token)
 
     # create account
     def create_user(self):
         user_data = {'email': 'user_test@exxpress.ma', 'username': 'user_test', 'name': 'user test','password':'Agadir414$'}
-        user = gl.users.create(user_data) 
+        user = self.gl.users.create(user_data) 
         #gl.users.delete(id=24)
-       
 
     def remove_user(self, user_id, user_name): # blocking them
         if user_id:
@@ -44,12 +38,41 @@ class GitlabLogger(Logger):
 
     def view_users(self):
         return self.gl.users.list(get_all=True)
-
+    
+    def get_token_expiration_date(self):
+        access_tokens = self.gl.personal_access_tokens.list()
+        #for token in access_tokens:
+            #if token.token == self.platform_api_token:
+                #return access_tokens[1].expires_at
+        return(access_tokens[2])
+    
 class MinioLogger(Logger):
-    pass
+    def __init__(self) -> None:
+        super().__init__()
+
+    def create_user(self):
+        pass
+    def rmeove_user(self):
+        pass
+    def view_users(self):
+        pass
+
+
+class MatterMostLogger(Logger):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def create_user(self):
+        pass
+    def rmeove_user(self):
+        pass
+    def view_users(self):
+        pass
+
+
+
     
     
 
 new = GitlabLogger("uPSVENLpMwJdC3sRLfJN",'http://gitlab.sys.infodat.com')
 
-print(new.view_users())
